b7adede73bfbde209d3b06e0b8dc3e00e908c609
MarcosPiardi <165842479+MarcosPiardi@users.noreply.github.com>
2025-07-25 18:45:00 -0300

    Após aulas da Hashtag montando projeto de questões

--- eventosmeta/eventos/views.py
+++ eventosmeta/eventos/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'Latest_question_list': latest_question_list}
+    return render(request, 'eventos/index.html', context)
+
+def details(request, question_id):
+    return HttpResponse('Essa é a pergunta de número %s' %question_id)
+
+def results(request, question_id):
+    return HttpResponse('Esses são os resultados da pergunta número %s' %question_id)
+
+def vote(request, question_id):
+    return HttpResponse('Você está votando na pergunta número %s' %question_id)
